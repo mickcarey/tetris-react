0d9fd4bca3c0254994092ca181df6c4ba03198c0
Michael Carey <contact@michaelcarey.com.au>
2021-06-02 10:44:31 +1000

    Initial commit

---
+++ src/components/Cell.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyledCell } from './styles/StyledCell';
+import { TETROMINOS } from '../tetrominos';
+
+const Cell = ({ type }) => {
+    console.log(type);
+
+    return (
+        <StyledCell type={type} color={TETROMINOS[type].color} />
+    );
+}
+
+export default React.memo(Cell);
